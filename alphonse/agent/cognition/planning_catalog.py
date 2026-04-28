49660677acaa4ce7278c5e0cd515ee131fa8c19e
khabaznak <khabaznak@gmail.com>
2026-04-28 09:28:57 -0600

    Remove unused cognition modules

--- alphonse/agent/cognition/planning_catalog.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from alphonse.agent.cognition.planning_engine import format_available_tool_catalog
-from alphonse.agent.cognition.planning_engine import format_available_tools
-
-__all__ = [
-    "format_available_tool_catalog",
-    "format_available_tools",
-]
