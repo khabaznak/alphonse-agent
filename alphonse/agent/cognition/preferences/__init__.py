2c4e8434e87b258f99d0c460d4c7754958c36cd0
khabaznak <khabaznak@gmail.com>
2026-05-07 08:10:43 -0600

    Remove no-op conversation profile layer

--- alphonse/agent/cognition/preferences/__init__.py
+++ alphonse/agent/cognition/preferences/__init__.py
@@ -9,12 +9,6 @@ from alphonse.agent.cognition.preferences.store import (
     set_preference,
     set_user_preference,
 )
-from alphonse.agent.cognition.preferences.conversation_profile import (
-    get_display_name,
-    get_locale,
-    set_display_name,
-    set_locale,
-)
 
 __all__ = [
     "get_user_preference",
@@ -24,8 +18,4 @@ __all__ = [
     "set_preference",
     "delete_preference",
     "get_with_fallback",
-    "get_display_name",
-    "set_display_name",
-    "get_locale",
-    "set_locale",
 ]
